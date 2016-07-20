e8d9ea7497260d7e3344550ab9072a80758bcf46
Drew Atkinson <drewatkinson5@gmail.com>
2016-07-20 17:01:20 -0400

    Add Chartist SASS files

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,5 @@
-var data = {
-  // A labels array that can contain any sort of values
-  labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
-  // Our series array that contains series objects or in this case series data arrays
-  series: [
-    [5, 2, 4, 2, 0]
-  ]
-};
-
-var tweetsChart = new Chartist.Line('#tweets', data);
-var sentimentChart = new Chartist.Line('#sentiment', data);
+var tweetsChart = new Chartist.Line('#tweets', {});
+var sentimentChart = new Chartist.Line('#sentiment', {});
 
 var updateGraphs = function() {
   $.get('/tweets/sentiment/hour', function(data) {
@@ -18,7 +9,6 @@ var updateGraphs = function() {
 };
 
 $(document).ready(function() {
-
-
     updateGraphs();
+    // TODO: When buttons are clicked, update with new param
 });

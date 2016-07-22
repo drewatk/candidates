8399d8b11e71abf0b7e468bc9e32a469c1732c80
Drew Atkinson <drewatkinson5@gmail.com>
2016-07-21 23:12:27 -0400

    change title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Candidates' });
+  res.render('index', { title: 'Candidates - Polling the 2016 Presidential Race with Twitter' });
 });
 
 module.exports = router;

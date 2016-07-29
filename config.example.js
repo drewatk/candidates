4122fa115773d5d890f38d46b11828a0e286525e
Drew Atkinson <drewatkinson5@gmail.com>
2016-07-29 08:37:29 -0400

    add config example, closes #4

---
+++ config.example.js
@@ -0,0 +1,18 @@
+var config = {};
+
+// Twitter API settings
+config.twitter = {
+  consumer_key: '',
+  consumer_secret: '',
+  access_token_key: '',
+  access_token_secret: ''
+};
+
+// pg-promise configuration object
+config.pgp = {
+  database: '',
+  user: '',
+  password: ''
+};
+
+module.exports = config;
